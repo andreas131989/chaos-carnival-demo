a7b6f8fac1624385152cc503e815083013580d75
Andreas Krivas <andreas.krivas@container-solutions.com>
2021-01-25 13:59:28 +0100

    Adding kube-prometheus submodule

--- infra/gcp/main.tf
+++ infra/gcp/main.tf
@@ -17,7 +17,7 @@ resource "google_container_node_pool" "addtional-nodes" {
   name       = "addtional-nodes"
   location   = "europe-west4-c"
   cluster    = google_container_cluster.primary.name
-  node_count = 3
+  node_count = 6
 
   node_config {
     preemptible  = true
